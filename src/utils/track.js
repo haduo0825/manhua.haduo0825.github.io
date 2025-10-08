0b58b4a6f51e522d478ffbe02d21bcc699718444
haduo <804171362@qq.com>
2025-10-08 09:09:26 +0800

    feat(跟踪): 添加设备指纹功能用于用户跟踪
    添加 @fingerprintjs/fingerprintjs 依赖以生成唯一设备指纹ID
    在 track.js 中集成指纹功能，将设备ID加入上报数据
    同时添加旧版 fingerprintjs 包以保持向后兼容

--- src/utils/track.js
+++ src/utils/track.js
@@ -1,15 +1,26 @@
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+const fpPromise = FingerprintJS.load();
+
 // 打点上报函数
 async function track(key, extraData = {}) {
     try {
+      const fp = await fpPromise;
+      const result = await fp.get();
+      
+      // 设备指纹ID
+      const deviceId = result.visitorId;
+
       const data = {
         key,
         data: {
           ip: await getIp(), // 获取IP地址
           ua: navigator.userAgent, // 获取用户UA
           time: new Date().toISOString(), // 获取当前时间
+          deviceId: deviceId, // 使用设备指纹ID
           ...extraData
         }
       };
+
   
       await fetch('https://explain-improved-mary-derby.trycloudflare.com/api/track', {
         method: 'POST',
